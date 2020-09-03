27e79f59b02260e7560fedf0fd87a6ffdb315eaa
Lucas Motizuki <motizuki@protonmail.com>
2020-09-03 14:29:47 -0300

    Criando seeders

---
+++ database/seeds/OrdemServicoSeeder.php
@@ -0,0 +1,16 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class OrdemServicoSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+    }
+}
---
+++ database/seeds/OrdemServicoTecnicoSeeder.php
@@ -0,0 +1,16 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class OrdemServicoTecnicoSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+    }
+}
