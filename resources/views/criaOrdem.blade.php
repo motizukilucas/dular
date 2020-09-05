74546255d851d3a966c02d9c8f3601cc440f1c22
Lucas Motizuki <motizuki@protonmail.com>
2020-09-04 21:54:56 -0300

    Styling botoes action

--- resources/views/criaOrdem.blade.php
+++ resources/views/criaOrdem.blade.php
@@ -29,13 +29,31 @@
                 </span>
             </nav>
 
-            <h2>
+            <h1>
                 Ordem de serviço <span class="badge badge-secondary">NOVA</span>
-            </h2>
+            </h1>
 
             <form id="cria-form" method="POST" action="/api/nova-ordem" class="needs-validation" novalidate>
                 @csrf
+                <button class="btn btn-primary" type="submit">Criar ordem</button>
             </form>
+
+            <div class="action">
+                <div class="btn-wpp btn-wrapper" style="margin-left: 0px;">
+                    <img class="btn-action" src="img/whatsapp-brands.svg">
+                    Whatsapp
+                </div>
+
+                <div class="btn-mail btn-wrapper">
+                    <img class="btn-action" src="img/envelope-solid.svg">
+                    Email
+                </div>
+
+                <div class="btn-print btn-wrapper">
+                    <img class="btn-action" src="img/print-solid.svg">
+                    Imprimir
+                </div>
+            </div>
         </div>
         <script type="text/javascript" src="js/carregaOrdemForm.js"></script>
         <script type="text/javascript" src="js/ordemFormHelper.js"></script>
