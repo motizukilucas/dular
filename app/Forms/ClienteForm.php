7bc71c136010ae18076168c0e8878df9d72f58a8
Lucas Motizuki <motizuki@protonmail.com>
2020-09-09 19:57:51 -0300

    Preenchendo com todos os estados

--- app/Forms/ClienteForm.php
+++ app/Forms/ClienteForm.php
@@ -78,10 +78,33 @@ class ClienteForm
                     <div class="col-md-3 mb-3">
                         <label>Estado</label>
                         <select name="estado" class="custom-select">
-                            <option selected disabled value="">Escolha...</option>
-                            <option>RJ</option>
-                            <option>SP</option>
-                            <option>...</option>
+                            <option selected value="RJ">RJ</option>
+                            <option value="SP">SP</option>
+                            <option value="AC">AC</option>
+                            <option value="AL">AL</option>
+                            <option value="AP">AP</option>
+                            <option value="AM">AM</option>
+                            <option value="BA">BA</option>
+                            <option value="CE">CE</option>
+                            <option value="ES">ES</option>
+                            <option value="GO">GO</option>
+                            <option value="MA">MA</option>
+                            <option value="MT">MT</option>
+                            <option value="MS">MS</option>
+                            <option value="MG">MG</option>
+                            <option value="PA">PA</option>
+                            <option value="PB">PB</option>
+                            <option value="PR">PR</option>
+                            <option value="PE">PE</option>
+                            <option value="PI">PI</option>
+                            <option value="RN">RN</option>
+                            <option value="RS">RS</option>
+                            <option value="RO">RO</option>
+                            <option value="RR">RR</option>
+                            <option value="SC">SC</option>
+                            <option value="SE">SE</option>
+                            <option value="TO">TO</option>
+                            <option value="DF">DF</option>
                         </select>
                         <div class="invalid-feedback">
                             Selecione um estado válido.
