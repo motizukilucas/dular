5549b9696530a02a8096e9a9ae8e7878d91705e5
Lucas Motizuki <motizuki@protonmail.com>
2020-10-16 10:20:23 -0300

    Revert "bind cliente repo e interface"
    This reverts commit c01fc797db25c9027ff71c4b08b6eebba7a337b0.

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -14,11 +14,6 @@ class AppServiceProvider extends ServiceProvider
     public function register()
     {
         $this->app->bind(
-            'App\Interfaces\ClienteRepositoryInterface',
-            'App\Repositories\ClienteRepository'
-        );
-
-        $this->app->bind(
             'App\Interfaces\FuncionarioRepositoryInterface',
             'App\Repositories\FuncionarioRepository'
         );
