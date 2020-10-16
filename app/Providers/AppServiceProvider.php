b3121d5f9f5260d56ac2b4629cb7f40854fc2bbc
Lucas Motizuki <motizuki@protonmail.com>
2020-10-16 10:35:34 -0300

    revert repos e interfaces

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -13,15 +13,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        $this->app->bind(
-            'App\Interfaces\FuncionarioRepositoryInterface',
-            'App\Repositories\FuncionarioRepository'
-        );
-
-        $this->app->bind(
-            'App\Interfaces\AparelhoRepositoryInterface',
-            'App\Repositories\AparelhoRepository'
-        );
+        //
     }
 
     /**
