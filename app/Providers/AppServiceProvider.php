2eeaee566ad14acbc052b17da87df025ac4db939
Lucas Motizuki <motizuki@protonmail.com>
2020-10-27 11:06:59 -0300

    Registrando o bind OSRepo e OSRepoInterce

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -13,7 +13,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        $this->app->bind(
+            'App\Interfaces\OrdemServicoRepositoryInterface',
+            'App\Repositories\OrdemServicoRepository'
+        );
     }
 
     /**
