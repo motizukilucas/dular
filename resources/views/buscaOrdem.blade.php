87cfb26f1958cd5d84a4a837f8c0b9b8fab0523e
Lucas Motizuki <motizuki@protonmail.com>
2020-10-27 11:06:11 -0300

    dando nome pro input query

--- resources/views/buscaOrdem.blade.php
+++ resources/views/buscaOrdem.blade.php
@@ -3,7 +3,7 @@
         <div class="container mt-3">
             <form id="buscaForm" class="form-inline">
                 @csrf
-                <input class="form-control mr-sm-2 col-md-4" type="search" placeholder="Buscar" aria-label="Buscar">
+                <input class="form-control mr-sm-2 col-md-4" name="query" type="search" placeholder="Buscar" aria-label="Buscar">
                 <button class="btn btn-outline-primary my-2 my-sm-0" type="submit">Buscar</button>
             </form>
         </div>
