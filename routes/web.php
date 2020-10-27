06899f0c5b79603c00a4a23e88dd04699ebdf2a1
Lucas Motizuki <motizuki@protonmail.com>
2020-10-27 09:58:46 -0300

    mudando a welcome para buscaOrdem

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('buscaOrdem');
 });
 
 Route::get('/cria-ordem', function () {
