4ac3ee57a7b26acc0d84ea053590f13482f337d0
Lucas Motizuki <motizuki@protonmail.com>
2020-11-08 16:24:52 -0300

    Adicionando margin top na tabela resultado

--- resources/views/buscaOrdem.blade.php
+++ resources/views/buscaOrdem.blade.php
@@ -6,8 +6,8 @@
                 <input class="form-control mr-sm-2 col-md-4" name="query" type="search" placeholder="Buscar" aria-label="Buscar">
                 <button class="btn btn-outline-primary my-2 my-sm-0" type="submit">Buscar</button>
             </form>
-
-            <table class="table table-hover">
+        
+            <table class="table table-hover mt-3">
                 <thead>
                     <tr>
                         <th scope="col">Ordem de Serviço</th>
