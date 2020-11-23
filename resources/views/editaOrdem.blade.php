96c562c45bba23ce588f303a8472eaedb02fef15
Lucas Motizuki <motizuki@protonmail.com>
2020-11-23 17:52:09 -0300

    Adicionando o form da OS no editar

--- resources/views/editaOrdem.blade.php
+++ resources/views/editaOrdem.blade.php
@@ -238,6 +238,8 @@
                 </div>
 
                 <!-- ordem servico tecnico form -->
+                <?php dump($os_tecnico[0]) ?>
+
                 <div class="form-row">
                     <div class="col-md-6 mb-3">
                         <label>Técnico responsável</label>
@@ -257,6 +259,79 @@
                         </div>
                     </div>
                  </div>
+
+                <!-- ordem servico form -->
+                <input type="hidden" name="id_ordem_servico" value="<?php echo $ordem_servico[0]->id ?>">
+
+                <div class="form-row">
+                    <div class="col-md-4 mb-3">
+                        <label>Valor</label>
+                        <div class="input-group">
+                            <div class="input-group-prepend">
+                                <div class="input-group-text">R$</div>
+                            </div>
+                            <input name="preco" value="<?php echo $ordem_servico[0]->preco ?>" type="text" class="form-control" required>    
+                        </div>
+                        <div class="invalid-feedback">
+                            Forneça um valor válido.
+                        </div>
+                    </div>
+
+                    <div class="col-md-4 mb-3">
+                        <label>Forma de pagamento</label>
+                        <select name="forma_pagamento" class="custom-select">
+                            <option selected  value="<?php echo $ordem_servico[0]->forma_pagamento ?>"><?php echo $ordem_servico[0]->forma_pagamento ?></option>
+                            <option value="DINHEIRO">Dinheiro</option>
+                            <option value="CARTAO">Cartão</option>
+                            <option value="BOLETO">Boleto</option>
+                            <option value="TRANSFERENCIA">Transferência</option>
+                        </select>
+                        <div class="invalid-feedback">
+                            Selecione um forma de pagamento válido.
+                        </div>
+                    </div>
+
+                    <div class="col-md-4 mb-3">
+                        <label>Desconto</label>
+                        <div class="input-group">
+                            <div class="input-group-prepend">
+                                    <div class="input-group-text">R$</div>
+                            </div>
+                            <input name="desconto" value="<?php echo $ordem_servico[0]->desconto ?>" type="text" class="form-control">
+                        </div>
+                        <div class="invalid-feedback">
+                            Forneça um desconto válido.
+                        </div>
+                    </div>
+                    </div>
+
+                    <div class="form-row">
+                    <div class="col-md-6 mb-3">
+                        <label>Atendente</label>
+                        <select name="atendente" class="custom-select" required>
+                        <option selected disabled>TODO</option>
+                        </select>
+                        <div class="invalid-feedback">
+                            Forneça um atendente válido.
+                        </div>
+                    </div>
+
+                    <div class="col-md-6 mb-3">
+                        <label>Status</label>
+                        <select name="status" class="custom-select" required>
+                            <option selected  value="<?php echo $ordem_servico[0]->status ?>"> <?php echo $ordem_servico[0]->status ?></option>
+                            <option value="AGUARDANDO">Aguardando aprovação</option>
+                            <option value="ENTREGUE">Entregue ao cliente</option>
+                            <option value="DEVOLUCAO">Devolução</option>
+                            <option value="SEM SOLUCAO">Entregue sem solução</option>
+                            <option value="FALTANDO PECAS">Faltando Peças</option>
+                            <option value="NA BANCADA">Na bancada</option>
+                        </select>
+                        <div class="invalid-feedback">
+                            Selecione um status válido.
+                        </div>
+                    </div>
+                </div>
 
                 <button class="btn btn-primary" type="submit">Atualiza ordem</button>
             </form>
