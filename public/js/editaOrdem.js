1c423b9be1e074f994baa078f9361ff6bdc18427
Lucas Motizuki <motizuki@protonmail.com>
2020-11-27 10:14:59 -0300

    Criando funcao de submit form

--- public/js/editaOrdem.js
+++ public/js/editaOrdem.js
@@ -1,28 +1,33 @@
-$("#edita-form").submit(function(event){
-    event.preventDefault();
-    
-    // atualiza cliente
-    dados_cliente = coletaDadosCliente();
-    dados_cliente.id = ($("[name='id_cliente']").val());
-    
-    cliente = atualizaCliente(dados_cliente);
+submitForm();
 
-    // atualiza aparelho
-    dados_aparelho = coletaDadosAparelho();
-    dados_aparelho.id = ($("[name='id_aparelho']").val());
+function submitForm()
+{
+    $("#edita-form").submit(function(event){
+        event.preventDefault();
+        
+        // atualiza cliente
+        dados_cliente = coletaDadosCliente();
+        dados_cliente.id = ($("[name='id_cliente']").val());
+        
+        cliente = atualizaCliente(dados_cliente);
 
-    aparelho = atualizaAparelho(dados_aparelho);
+        // atualiza aparelho
+        dados_aparelho = coletaDadosAparelho();
+        dados_aparelho.id = ($("[name='id_aparelho']").val());
 
-    // atualiza os aparelho
-    dados_os_aparelho = coletaDadosOrdemServicoAparelho();
-    dados_os_aparelho.id = ($("[name='id_os_aparelho']").val());
+        aparelho = atualizaAparelho(dados_aparelho);
 
-    os_aparelho = atualizaOsAparelho(dados_os_aparelho);
+        // atualiza os aparelho
+        dados_os_aparelho = coletaDadosOrdemServicoAparelho();
+        dados_os_aparelho.id = ($("[name='id_os_aparelho']").val());
 
-    // atualiza os peca
-    dados_os_peca = coletaDadosOrdemServicoPeca();
-    dados_os_peca.id = ($("[name='id_os_peca']").val());
+        os_aparelho = atualizaOsAparelho(dados_os_aparelho);
 
-    os_peca = atualizaOsPeca(dados_os_peca);
+        // atualiza os peca
+        dados_os_peca = coletaDadosOrdemServicoPeca();
+        dados_os_peca.id = ($("[name='id_os_peca']").val());
 
-});
+        os_peca = atualizaOsPeca(dados_os_peca);
+
+    });
+}
